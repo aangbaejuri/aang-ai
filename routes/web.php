294289a0d9bdceb15f46f9ad6e6939bcd02d17a3
Aang Baejuri <aangbaejuri.dev@gmail.com>
2025-11-19 18:14:37 +0700

    Fix route definition for clearing chats

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,3 @@ Route::get('/chat/new', [ChatbotController::class, 'newChat'])->name('chat.new')
 Route::get('/chat/switch/{id}', [ChatbotController::class, 'switchChat'])->name('chat.switch');
 Route::get('/chat/delete/{id}', [ChatbotController::class, 'deleteChat'])->name('chat.delete');
 Route::get('/chat/clear', [ChatbotController::class, 'clearAllChats'])->name('chat.clear');
-
-Route::post('/chat/model/switch', [ChatbotController::class, 'switchModel'])->name('chat.model.switch');
